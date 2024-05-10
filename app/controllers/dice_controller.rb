254c3f343ce923ae867358b7a29ca45020cd5b74
Angeles Guyot <angeles.guyot@kellogg.northwestern.edu>
2024-05-10 19:26:25 +0000

    aa

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,6 +4,7 @@ class DiceController < ApplicationController
     @die1 = rand(1..6)
     @die2 = rand(1..6)
     @total = @die1 + @die2
+    @name = params["name"] # @name es cualquier cosa que ponga como name
   end
 
 end
